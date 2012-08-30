323c0f9f3e10418e2c6f9e193c077411fa2924c6
Abe Petrillo <abe.petrillo@gmail.com>
2012-08-31 00:53:25 +0100

    Getting started:
     - Standalone javascript framework added (Jasmine)
     - Namespace capability with tests
     - run_specs.html to actually run the tests

---
+++ spec/namespace_spec.js
@@ -0,0 +1,26 @@
+describe('namespace',function(){
+  describe('when the page is loaded',function(){
+    it('should define the App object in the global namespace',function(){
+      expect(window.App).toBeDefined();
+      expect(App).toBeDefined();
+    });
+  });
+  describe('#extend',function(){
+    it('should define a child object within the namespace', function(){
+      App.extend('house');
+      expect(App.house).toBeDefined();
+    });
+    it('should not over-ride a namespace',function(){
+      App.house = {
+        door_number : 4
+      }
+      App.extend('house');
+      expect(App.house.door_number).toBe(4);
+    });
+    it('should handle the specification of "App" within the namespace',function(){
+      App.extend('App.someNamespace',function(){
+        expect(App.someNamespace).toBeDefined();
+      });
+    });
+  });
+})
